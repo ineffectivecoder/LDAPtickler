4798effb767432058ed7da11680481e1ea3e3e89
silversurfer <r2d2@sostup.id>
2025-07-18 15:49:42 +0000

    removed debugging statements from removedconstraineddelegation

--- goldapquery.go
+++ goldapquery.go
@@ -536,11 +536,7 @@ func (c *Conn) RemoveConstrainedDelegation(username string, spn string) error {
 			}
 		}
 	}
-	fmt.Printf("%s\n", delegationresstr)
-	/*uac, err := flagset(uacstr, UACTrustedForDelegation)
-	if err != nil {
-		return err
-	}*/
+	//fmt.Printf("%s\n", delegationresstr)
 	enableReq := ldap.NewModifyRequest(dn, []ldap.Control{})
 	enableReq.Replace("msDS-AllowedToDelegateTo", updatedSPNs)
 	return c.lconn.Modify(enableReq)
